b92aebcadcca8d7441e25e9dd2a3a8513d98793b
Emmanuel Murairi <emurairi18@alastudents.org>
2022-03-04 16:25:33 +0400

    evaluate result

--- calculator.js
+++ calculator.js
@@ -1,5 +1,21 @@
 
-function calculate() {
-  var userInput = "4+4";
-  console.log(eval(userInput))
-}
+document.querySelectorAll('button').forEach(item => {
+
+  if (item.innerHTML == '=') {
+    item.addEventListener('click', () => {
+
+    
+      var result = eval(document.querySelector('#result').value);
+      document.querySelector('#result').value = result;
+
+    })
+  }
+  else{
+    item.addEventListener('click', event => {
+        
+      var input = event.target.innerText;
+      document.querySelector('#result').value += input;
+    
+    })
+  }  
+})
